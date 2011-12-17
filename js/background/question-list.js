631d102085e366bff8265fbf660b16b77088637f
Thomas Taschauer <tomtasche@gmail.com>
2011-12-17 11:44:15 +0100

    add support for all currently available stackexchange sites

--- js/background/question-list.js
+++ js/background/question-list.js
@@ -73,6 +73,294 @@ st.NETWORK_INFO = {
   'gamedev': {
     name: 'Game Development',
     root: 'gamedev.stackexchange.com'
+  },
+  {
+    name: 'Super User',
+    root: 'superuser.com'
+  },
+  {
+    name: 'Server Fault',
+    root: 'serverfault.com'
+  },
+  {
+    name: 'Ask Ubuntu',
+    root: 'askubuntu.com'
+  },
+  {
+    name: 'Gaming',
+    root: 'gaming.stackexchange.com'
+  },
+  {
+    name: 'Programmers',
+    root: 'programmers.stackexchange.com'
+  },
+  {
+    name: 'Mathematics',
+    root: 'math.stackexchange.com'
+  },
+  {
+    name: 'Apple',
+    root: 'apple.stackexchange.com'
+  },
+  {
+    name: 'English Language and Usage',
+    root: 'english.stackexchange.com'
+  },
+  {
+    name: 'TeX - LaTeX',
+    root: 'tex.stackexchange.com'
+  },
+  {
+    name: 'Android Enthusiasts',
+    root: 'android.stackexchange.com'
+  },
+  {
+    name: 'WordPress',
+    root: 'wordpress.stackexchange.com'
+  },
+  {
+    name: 'Unix and Linuxr',
+    root: 'unix.stackexchange.com'
+  },
+  {
+    name: 'Web Applications',
+    root: 'webapps.stackexchange.com'
+  },
+  {
+    name: 'OnStartups',
+    root: 'answers.onstartups.com'
+  },
+  {
+    name: 'SharePoint',
+    root: 'sharepoint.stackexchange.com'
+  },
+  {
+    name: 'Webmasters',
+    root: 'webmasters.stackexchange.com'
+  },
+  {
+    name: 'Cooking',
+    root: 'cooking.stackexchange.com'
+  },
+  {
+    name: 'Statistical Analysis',
+    root: 'stats.stackexchange.com'
+  },
+  {
+    name: 'Electrical Engineering',
+    root: 'electronics.stackexchange.com'
+  },
+  {
+    name: 'Photography',
+    root: 'photo.stackexchange.com'
+  },
+  {
+    name: 'Drupal Answers',
+    root: 'drupal.stackexchange.com'
+  },
+  {
+    name: 'GIS',
+    root: 'gis.stackexchange.com'
+  },
+  {
+    name: 'Physics',
+    root: 'physics.stackexchange.com'
+  },
+  {
+    name: 'Database Administrators',
+    root: 'dba.stackexchange.com'
+  },
+  {
+    name: 'Code Review',
+    root: 'codereview.stackexchange.com'
+  },
+  {
+    name: 'Home Improvement',
+    root: 'diy.stackexchange.com'
+  },
+  {
+    name: 'IT Security',
+    root: 'security.stackexchange.com'
+  },
+  {
+    name: 'Theoretical Computer Science',
+    root: 'cstheory.stackexchange.com'
+  },
+  {
+    name: 'Personal Finance and Money',
+    root: 'money.stackexchange.com'
+  },
+  {
+    name: 'Skeptics',
+    root: 'skeptics.stackexchange.com'
+  },
+  {
+    name: 'Role-playing Games',
+    root: 'rpg.stackexchange.com'
+  },
+  {
+    name: 'Stack Apps',
+    root: 'stackapps.com'
+  },
+  {
+    name: 'Science Fiction and Fantasy',
+    root: 'scifi.stackexchange.com'
+  },
+  {
+    name: 'Jewish Life and Learning',
+    root: 'judaism.stackexchange.com'
+  },
+  {
+    name: 'Bicycles',
+    root: 'bicycles.stackexchange.com'
+  },
+  {
+    name: 'Graphic Design',
+    root: 'graphicdesign.stackexchange.com'
+  },
+  {
+    name: 'Fitness and Nutrition',
+    root: 'fitness.stackexchange.com'
+  },
+  {
+    name: 'Writers',
+    root: 'writers.stackexchange.com'
+  },
+  {
+    name: 'Code Golf',
+    root: 'codegolf.stackexchange.com'
+  },
+  {
+    name: 'Board and Card Games',
+    root: 'boardgames.stackexchange.com'
+  },
+  {
+    name: 'Homebrew',
+    root: 'homebrew.stackexchange.com'
+  },
+  {
+    name: 'Project Management',
+    root: 'pm.stackexchange.com'
+  },
+  {
+    name: 'Musical Practice and Performance',
+    root: 'music.stackexchange.com'
+  },
+  {
+    name: 'Parenting',
+    root: 'parenting.stackexchange.com'
+  },
+  {
+    name: 'Travel',
+    root: 'travel.stackexchange.com'
+  },
+  {
+    name: 'Audio-Video Production',
+    root: 'avp.stackexchange.com'
+  },
+  {
+    name: 'Quantitative Finance',
+    root: 'quant.stackexchange.com'
+  },
+  {
+    name: 'Christianity',
+    root: 'christianity.stackexchange.com'
+  },
+  {
+    name: 'Japanese Language and Usage',
+    root: 'japanese.stackexchange.com'
+  },
+  {
+    name: 'Software Quality Assurance and Testing',
+    root: 'sqa.stackexchange.com'
+  },
+  {
+    name: 'Motor Vehicle Maintenance and Repair',
+    root: 'mechanics.stackexchange.com'
+  },
+  {
+    name: 'Personal Productivity',
+    root: 'productivity.stackexchange.com'
+  },
+  {
+    name: 'German Language and Usage',
+    root: 'german.stackexchange.com'
+  },
+  {
+    name: 'Gardening and Landscaping',
+    root: 'gardening.stackexchange.com'
+  },
+  {
+    name: 'Philosophy',
+    root: 'philosophy.stackexchange.com'
+  },
+  {
+    name: 'Cryptography',
+    root: 'crypto.stackexchange.com'
+  },
+  {
+    name: 'Bitcoin',
+    root: 'bitcoin.stackexchange.com'
+  },
+  {
+    name: 'Astronomy',
+    root: 'astronomy.stackexchange.com'
+  },
+  {
+    name: 'French Language and Usage',
+    root: 'french.stackexchange.com'
+  },
+  {
+    name: 'Theoretical Physics',
+    root: 'theoreticalphysics.stackexchange.com'
+  },
+  {
+    name: 'Literature',
+    root: 'literature.stackexchange.com'
+  },
+  {
+    name: 'Linguistics',
+    root: 'linguistics.stackexchange.com'
+  },
+  {
+    name: 'Signal Processing',
+    root: 'dsp.stackexchange.com'
+  },
+  {
+    name: 'LEGO® Answers',
+    root: 'bricks.stackexchange.com'
+  },
+  {
+    name: 'History',
+    root: 'history.stackexchange.com'
+  },
+  {
+    name: 'Spanish Language and Usage',
+    root: 'spanish.stackexchange.com'
+  },
+  {
+    name: 'Economics',
+    root: 'economics.stackexchange.com'
+  },
+  {
+    name: 'Computational Science',
+    root: 'scicomp.stackexchange.com'
+  },
+  {
+    name: 'Biblical Hermeneutics',
+    root: 'hermeneutics.stackexchange.com'
+  },
+  {
+    name: 'Healthcare IT',
+    root: 'healthcareit.stackexchange.com'
+  },
+  {
+    name: 'Firearms',
+    root: 'firearms.stackexchange.com'
+  },
+  {
+    name: 'Movies',
+    root: 'movies.stackexchange.com'
   }
 };
 
